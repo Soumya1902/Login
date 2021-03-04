202f716f756f5084da9beda1cbc0190e1dc516c9
thotabharath <thotabharathkumar328@gmail.com>
2021-03-04 12:28:52 +0530

    this is my first commit

---
+++ Addition/src/Sum.java
@@ -0,0 +1,12 @@
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 456 , b = 654;
+int sum;
+sum=a+b;
+System.out.println("Sum of these number is: " +sum);
+	}
+
+}
